e9882530950738e239b4cdf1208f898299fa49e0
Béatrice <beatrice.briand@viacesi.fr>
2023-03-23 16:13:17 +0100

    ajout app setting pgadmin

--- data.tf
+++ data.tf
@@ -6,3 +6,24 @@ data "azurerm_key_vault" "kv" {
 	name = "kv-${var.project_name}${var.environment_suffix}"
 	resource_group_name = data.azurerm_resource_group.rg-maalsi.name
 }
+
+data "azurerm_key_vault_secret" "database-login" {
+	name = "database-login"
+	key_vault_id = data.azurerm_key_vault.kv.id
+}
+
+data "azurerm_key_vault_secret" "database-password" {
+	name = "database-password"
+	key_vault_id = data.azurerm_key_vault.kv.id
+}
+
+data "azurerm_key_vault_secret" "pgadminDefaultEmail" {
+	name = "pgadminDefaultEmail"
+	key_vault_id = data.azurerm_key_vault.kv.id
+}
+
+data "azurerm_key_vault_secret" "pgadminDefaultPwd" {
+	name = "pgadminDefaultPwd"
+	key_vault_id = data.azurerm_key_vault.kv.id
+}
+
--- main.tf
+++ main.tf
@@ -26,23 +26,93 @@ resource "azurerm_postgresql_server" "pgsql-srv" {
   location            = data.azurerm_resource_group.rg-maalsi.location
   resource_group_name = data.azurerm_resource_group.rg-maalsi.name
 
-  sku_name = "B_Gen5_2"
+  administrator_login          = data.azurerm_key_vault_secret.database-login.value #"psqladmin"
+  administrator_login_password = data.azurerm_key_vault_secret.database-password.value #"H@Sh1CoR3!"
+  
+  sku_name   = "GP_Gen5_4"
+  version    = "11"
+  storage_mb = 640000
 
-  storage_mb                   = 5120
   backup_retention_days        = 7
-  geo_redundant_backup_enabled = false
+  geo_redundant_backup_enabled = true
   auto_grow_enabled            = true
 
-  administrator_login          = "psqladmin"
-  administrator_login_password = "H@Sh1CoR3!"
-  version                      = "9.5"
-  ssl_enforcement_enabled      = true
+  public_network_access_enabled    = false
+  ssl_enforcement_enabled          = false
+  ssl_minimal_tls_version_enforced = "TLSEnforcementDisabled"
+
+  
 }
 
-resource "azurerm_postgresql_database" "pgsql-db" {
-  name                = "pgsql-db-${var.project_name}${var.environment_suffix}"
+#########################
+#  API Web App
+#########################
+resource "azurerm_service_plan" "app-plan" {
+    name="plan-${var.project_name}${var.environment_suffix}"
+    resource_group_name = data.azurerm_resource_group.rg-maalsi.name
+    location = data.azurerm_resource_group.rg-maalsi.location
+	sku_name            = "P1v2"
+    os_type             = "Linux"  
+}
+
+resource "azurerm_linux_web_app" "web-app" {
+  name = "web-${var.project_name}${var.environment_suffix}"
   resource_group_name = data.azurerm_resource_group.rg-maalsi.name
-  server_name         = azurerm_postgresql_server.pgsql-srv.name
-  charset             = "UTF8"
-  collation           = "English_United States.1252"
+  location = data.azurerm_resource_group.rg-maalsi.location
+  service_plan_id = azurerm_service_plan.app-plan.id
+
+  site_config {
+	 always_on = true
+	 application_stack {
+	   node_version = "16-lts"
+	 }
+  }
+
+  app_settings = {
+	// "DB_USERNAME" = "${data.azurerm_key_vault_secret.db-username.value}@${azurerm_postgresql_server.pgsql-srv.name}"
+    PORT=3000
+    DB_HOST=""
+    DB_USERNAME="YOUR_POSTGRES_USERNAME"
+    DB_PASSWORD="YOUR_POSTGRES_PASSWORD"
+    DB_DATABASE="YOUR_POSTGRES_DATABASE"
+    DB_DAILECT="postgres"
+    DB_PORT=5432
+    ACCESS_TOKEN_SECRET = "YOUR_SECRET_KEY"
+    REFRESH_TOKEN_SECRET = "YOUR_SECRET_KEY"
+    ACCESS_TOKEN_EXPIRY = "15m"
+    REFRESH_TOKEN_EXPIRY = "7d"
+    REFRESH_TOKEN_COOKIE_NAME = "jid"
+  }
+}
+
+#################################
+#  PGAdmin : Container instance
+#################################
+
+resource "azurerm_container_group" "pgadmin" {
+  name                = "aci-pgadmin-${var.project_name}${var.environment_suffix}"
+  resource_group_name = data.azurerm_resource_group.rg-maalsi.name
+  location            = data.azurerm_resource_group.rg-maalsi.location
+  ip_address_type     = "Public"
+  dns_name_label      = "aci-pgadmin-${var.project_name}${var.environment_suffix}"
+  os_type             = "Linux"
+  exposed_port        = []
+
+  container {
+    name   = "pgadmin"
+    image  = "dpage/pgadmin4:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+ports {
+  port = 80
+  protocol = "TCP"
+
+}
+
+    environment_variables = {
+	  "PGADMIN_DEFAULT_EMAIL" = data.azurerm_key_vault_secret.pgadminDefaultEmail.value
+	  "PGADMIN_DEFAULT_PASSWORD" = data.azurerm_key_vault_secret.pgadminDefaultPwd.value
+    }
+  }
 }
